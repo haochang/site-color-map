aec3d7a5130d2038acd3e103bfc364a0649311df
Hao Chang <hao.chang116@gmail.com>
2018-10-22 22:50:52 +0100

    Batter background color

--- src/component/ColorsOutput.js
+++ src/component/ColorsOutput.js
@@ -10,8 +10,11 @@ const styles = {
         height: 'auto',
         padding: 40,
     },
+    color: {
+        margin: '15px 0',
+    },
     bar: {
-        height: 15,
+        height: 20,
         position: 'relative',
         overflow: 'hidden',
     },
@@ -30,15 +33,23 @@ const styles = {
 class ColorsOutput extends Component {
     state = {}
 
+    hexToRgb = (hex) => {
+        const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        const newHex = hex.replace(shorthandRegex, function(m, r, g, b) {
+            return r + r + g + g + b + b;
+        });
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(newHex);
+        return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
+    }
+
     render() {
         const { classes } = this.props;
         return (
             <Paper className={classes.paper}>
                 {Object.keys(this.props.colors).map((color, index) =>
-                    <div key={index}>
-                        <div > {color}</div>
-                        <div> {this.props.colors[color]}</div>
-                        <div style={{backgroundColor: 'rgb(255, 140, 179)' }} className={classes.bar}>
+                    <div key={index} className={classes.color}>
+                        <div > {color}  <b>{this.props.colors[color]}</b></div>
+                        <div style={{backgroundColor: `rgba(${this.hexToRgb(color)}, 0.2)` }} className={classes.bar}>
                             <div className={classes.innerBar}
                                 style={{transform: `scaleX(${this.props.colors[color]/10})`,
                                     backgroundColor: `${color}`}}>
